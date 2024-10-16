3650332b5ae98e8dd55e75a437aeac3bd3416299
Paula Olier <paulaolier@gmail.com>
2024-10-15 19:19:33 -0500

    creacion constructor para inicializar un cliente

--- cliente.cs
+++ cliente.cs
@@ -9,4 +9,13 @@ namespace Taller4
         public string Correo { get; set; } // Correo electrónico del cliente
         public DateTime FechaRegistro { get; set; } // Fecha de registro del cliente
 
+        // Constructor para inicializar un cliente
+        public Cliente(int id, string nombre, string correo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Correo = correo;
+            FechaRegistro = DateTime.Now; // Asigna la fecha de registro automáticamente
+        }
+
 
