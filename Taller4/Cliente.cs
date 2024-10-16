68d475039402e1ad36f6f9d86c5cff396183a060
Paula Olier <paulaolier@gmail.com>
2024-10-15 19:33:38 -0500

    edicion clientes

--- Taller4/Cliente.cs
+++ Taller4/Cliente.cs
@@ -1,21 +1,18 @@
-using System;
-
 namespace Taller4
 {
     public class Cliente
     {
-        public int Id { get; set; } // Identificador único del cliente
-        public string Nombre { get; set; } // Nombre del cliente
-        public string Correo { get; set; } // Correo electrónico del cliente
-        public DateTime FechaRegistro { get; set; } // Fecha de registro del cliente
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaRegistro { get; set; }
 
-        // Constructor para inicializar un cliente
         public Cliente(int id, string nombre, string correo)
         {
             Id = id;
             Nombre = nombre;
             Correo = correo;
-            FechaRegistro = DateTime.Now; // Asigna la fecha de registro automáticamente
+            FechaRegistro = DateTime.Now;
         }
 
         // Método para mostrar información del cliente
