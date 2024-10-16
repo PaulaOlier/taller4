43d396137092bb86a42b77985d1d7a1fd77270d2
Paula Olier <paulaolier@gmail.com>
2024-10-15 19:18:45 -0500

    creacion set y get en cliente

---
+++ cliente.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Taller4
+{
+    public class Cliente
+    {
+        public int Id { get; set; } // Identificador único del cliente
+        public string Nombre { get; set; } // Nombre del cliente
+        public string Correo { get; set; } // Correo electrónico del cliente
+        public DateTime FechaRegistro { get; set; } // Fecha de registro del cliente
+
+
